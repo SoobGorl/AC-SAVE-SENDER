1fe5b2c97a8c29d59e6658c8cb8563473651e5c2
Soup Girl <72813628+SoobGorl@users.noreply.github.com>
2025-08-20 21:05:18 -0400

    header and subheader checking

--- resetflag.py
+++ resetflag.py
@@ -4,7 +4,14 @@ import logging
 import pathlib
 import sys
 
-# TODO! check if file has animal crossing header
+# TODO! check flags ONLY after validity
+
+# security will be a larger issue but these are basic checks to see if the file is mostly an AC file
+"""check after file is recieved if safe
+parse entire file and make sure its correct and cant execute bad stuff
+when you recieve a file (clientside) verify that file is safe
+if all verifications succeed and is valid data/save file, then use it"""
+
 
 # ------------ DEBUG ------------
 debug = True # prints out a bunch of debug info if True
@@ -16,65 +23,54 @@ save_likelyhood = { # defines what the file_trust number means for humans
     0: "NOT A VALID FILE",
     1: "VALID .GCI FILE",
     2: "VALID RESET ADDRESSES",
-    3: "VALID GAME HEADER", # not yet implemented
-    4: "UNKNOWN FILE, SEMI-VALID"
+    3: "VALID GAME HEADER",
+    4: "VALID AC SUBHEADER",
+    -1: "UNKNOWN FILE, SEMI-VALID"
 }
 def trust_reporter(): # prints out trust number, then the trust message/state
     print("SAVE LIKELYHOOD: " + str(file_trust) + ", " + save_likelyhood[file_trust])
 
 # -------- FILE OPENING ---------
-#root = tk.Tk() # creates file-specific open dialog through tkinter
-#root.withdraw() # (credit to tomvodi/dimakin on stack exchange for this script)
-#file_path = filedialog.askopenfilename() # prompts user to select file
-#file_extension = pathlib.Path(file_path).suffix # gets the file extension from file in filepath
-#acsavefile = open(file_path, mode = "rb") # opens selected file in read binary mode + names it as a variable
-acsavefile = open("RESETFLAG.gci", mode = "rb")
+root = tk.Tk() # creates file-specific open dialog through tkinter
+root.withdraw() # (credit to tomvodi/dimakin on stack exchange for this script)
+file_path = filedialog.askopenfilename() # prompts user to select file
+file_extension = pathlib.Path(file_path).suffix # gets the file extension from file in filepath
+acsavefile = open(file_path, mode = "rb") # opens selected file in read binary mode + names it as a variable
+
+
+
+
 
 
 
 def bytes_to_string(address, byte_length):
     acsavefile.seek(address)
-    return acsavefile.read(byte_length).decode('UTF-8')
+    return acsavefile.read(byte_length)
 
+if debug:
+    print("FILE'S GAMEID HEADER: " + str(bytes_to_string(0x0, 15)))
+    print("FILE'S 'AC' SUBHEADER: " + str(bytes_to_string(0x40, 15)))
+    print("FILE'S TOWN NAME: " + str(bytes_to_string(0x60, 8)))
 
+if bytes_to_string(0x0, 25) == b'GAFE01\xff\x01DobutsunomoriP_MU':
+    file_trust = 3
+    if debug:
+        # print("The GAMEID header is correct.")
+        trust_reporter()
+else:
+    file_trust = 0
+    if debug:
+        logging.warning("GAMEID HEADER DOES NOT MATCH!")
 
-print(bytes_to_string(0x0, 25))
-
-#if str(acsavefile.read(25)) == "b'GAFE01\xff\x01DobutsunomoriP_MU'":
-#    print("check good")
-#else:
-#    print("check bad")
-
-acsavefile.seek(0x40)
-actext_check = acsavefile.read(15)
-
-acsavefile.seek(0x60)
-acsavefile.read(8)
-
-
-#print(header_check)
-#if actext_check == "GAFE01\xff\x01DobutsunomoriP_MU":
-#    print("header has passed check")
-#else:
-#    "header not passed"
-#print(actext_check)
-#if actext_check == "Animal Crossing":
-#    print("animal crossing subheader passed")
-#else:
-#    "subheader not passed"
-
-sys.exit()
-# look for "ANIMAL CROSSING" text
-# look for town name
-# look for villagers
-# separate file validity checks from flag checking
-# check flags ONLY after validity
-
-# security will be a larger issue but these are basic checks to see if the file is mostly an AC file
-"""check after file is recieved if safe
-parse entire file and make sure its correct and cant execute bad stuff
-when you recieve a file (clientside) verify that file is safe
-if all verifications succeed and is valid data/save file, then use it"""
+if bytes_to_string(0x40, 15) == b'Animal Crossing':
+    file_trust = 4
+    if debug:
+        # print("The 'AC' subheader is correct.")
+        trust_reporter()
+else:
+    file_trust = 0
+    if debug:
+        logging.warning("AC SUBHEADER DOES NOT MATCH!")
 
 # -------- GCI HANDLING ---------
 valid_gci = False # sets initial value to False, as it hasn't been checked yet
@@ -84,15 +80,12 @@ if file_extension == ".gci": # if the selected file has the extension .gci, then
 else:
     file_trust = 0 # for safety :)
     logging.critical("SELECTED FILE DID NOT CONTAIN .GCI EXTENSION")
-    pass
 
 # ------------ DEBUG ------------ # may be redundant...
 if debug: # reports if file is a .GCI
     print("Chosen file = " + file_path)
     # print("Is .GCI? = " + str(valid_gci))
     trust_reporter()
-else:
-    pass
 
 # --- DEFINE RESET ADDRESSES ---
 reset_address = {
@@ -106,8 +99,6 @@ reset_address = {
 if debug: # prints all addresses
     for address in reset_address.values():
         print(hex(address))
-else:
-    pass
 
 # ------ READ ADDRESS VALUE ------
 def reset_flag(reset_byte): # function reads the given address in its argument, and converts the value inside into a number
@@ -140,7 +131,7 @@ for address in reset_address:
     if reset_flag(reset_address[address]) == 0:
         reset_active = False
     elif error_flag:
-        logging.critical("UNSPECIFIED ERROR. MOST LIKELY, RESET ADDRESS WAS NOT FOUND.")
+        logging.warning("UNSPECIFIED ERROR. MOST LIKELY, RESET ADDRESS WAS NOT FOUND.")
         break # if error tripped, stop and break here
     else:
         reset_active = True
@@ -148,8 +139,6 @@ for address in reset_address:
 # ------------ DEBUG ------------
 if debug:
     trust_reporter() # print current trust state
-else:
-    pass
 
 # ------- RESET REPORTING --------
 if reset_active:
@@ -161,7 +150,5 @@ else:
     print("The save is currently inactive/closed. RESET_ACTIVE STATE: " + str(reset_active))
 
 # ------ EDGECASE ERROR, NOT GCI BUT SUCCEEDED ALL OTHER CHECKS ------
-if not valid_gci and file_trust == 2:
+if not valid_gci and file_trust == -1:
     logging.warning("INVALID .GCI EXTENSION. GAME SHARK SAVE? SUCCESSFULLY RAN ALL OTHER TESTS.")
-else:
-    pass # REMOVE THIS ENTIRE THING WHEN THE ACTUAL CHECK WORKS,
